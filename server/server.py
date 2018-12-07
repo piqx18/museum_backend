9d8d4541851172c8004658ba694723e2508a8705
piqx18 <roma.druzhinin@hotmail.com>
2018-12-07 22:15:34 +0700

    правка адреса ручек

--- server/server.py
+++ server/server.py
@@ -7,13 +7,13 @@ from museum_backend.server.list_actions.users.Users import Users
 class Server(object):
 
     @staticmethod
-    @post("/api/v1/authorization")
+    @post("/api/v1/user/authorization")
     def auth():
         data = request.body.read()
         return Auth().auth(data=data)
 
     @staticmethod
-    @post("/api/v1/createuser")
+    @post("/api/v1/user/createuser")
     def create_user():
         data = request.body.read()
         return Users().create_user(data=data)
